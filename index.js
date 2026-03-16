7a55574c0028ba20ea0b0395faf4f563397e4cd3
Shivansh Pandey <shivanshp987@gmail.com>
2026-03-16 13:25:59 +0530

    Remove Express server and migrate package.json to Next.js
    Delete index.js, src/server.js, public/index.html. Replace Express/dotenv deps with Next.js, React, radix-ui, shadcn, and Tailwind.

--- index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./src/server");
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`GitWall server running on http://localhost:${PORT}`);
-});
